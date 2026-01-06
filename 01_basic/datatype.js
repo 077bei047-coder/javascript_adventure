7fc5032eaf6d7eeaf7e3ad970579b804fa803f19
077bei047-coder <077bei047@ioepc.edu.np>
2026-01-06 11:02:00 +0000

    container config done

---
+++ 01_basic/datatype.js
@@ -0,0 +1,18 @@
+"use strict";//it says computer to use new version of js
+
+//alert( 3 + 3 ) it run and give 6 ans in brower but in todays generation it use nodejs and doesn't operate and cannot run this code
+let name="Arati"//string datatype
+let age= 23//number datatype
+let isLoggedIn= false//boolean datatype
+
+// number=> 2 to power 53
+//bigint
+//boolean=> true/false
+//null =>standalone value
+//undefined =>
+//symbol=>uniqueness
+
+//object
+
+//null is a object datatype//it is an interview qns//
+console.log(typeof null);//this syntax help to identify the datatype of any //
