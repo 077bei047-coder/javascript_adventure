6b9754f45e9a5db713c6d026f725d38b7ad22956
077bei047-coder <077bei047@ioepc.edu.np>
2026-01-11 16:51:29 +0000

    container config done

---
+++ datatype_summary.js
@@ -0,0 +1,23 @@
+//primitive
+//In JavaScript, a primitive (primitive value, primitive data type) is data that is not an object and has no methods or properties.
+//All primitives are immutable; that is, they cannot be altered.
+//The variable may be reassigned to a new value, but the existing value can not be changed in the ways that objects, arrays, and functions can be altered.
+// There are 7 primitive data types:
+
+//string
+//number
+//bigint
+//boolean
+//undefined
+//symbol
+//null
+
+//non primitive or reference types
+// They can store collections of values or more complex entities.
+//they are:
+//Object,Array,functions
+
+
+//javaScript is a dynamically typed language
+//You do not need to declare the data type of a variable.
+//The type is decided at runtime, and it can change while the program is running.
