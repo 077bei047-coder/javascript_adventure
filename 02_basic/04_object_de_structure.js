1fb6e03d3782b8e4a9a64f45e6aaa30cb151104c
077bei047-coder <077bei047@ioepc.edu.np>
2026-01-23 10:23:12 +0000

    json talk

---
+++ 02_basic/04_object_de_structure.js
@@ -0,0 +1,17 @@
+const course={
+    coursename:"science",
+    price:"999",
+    courseteachername:"Arati"
+}
+//aaba course vitra ko price lai detect grne kasari
+const{price}=course
+console.log(price);
+
+//API example
+//{
+   // name: "Arati",
+    //invest: "999",
+    //teachername: "AAratti"
+
+
+//}
